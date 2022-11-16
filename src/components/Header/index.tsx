ba12091b6b83799b6220e5780d8967066468fb4a
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-11-16 18:16:24 -0300

    feat: :sparkles: creating new acessible dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoImg from '../../assets/logo.svg'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
@@ -7,7 +9,21 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
